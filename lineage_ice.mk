49f88f2babe8378cb9af0f5be4db4023802edb08
Liquid <101341072+Liquid7z@users.noreply.github.com>
2025-06-24 23:37:59 +0530

    Update lineage_ice.mk

--- lineage_ice.mk
+++ lineage_ice.mk
@@ -12,7 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/realme/ice/device.mk)
 
 # Inherit some common infinity stuff.
-$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_ice
 PRODUCT_DEVICE := ice
