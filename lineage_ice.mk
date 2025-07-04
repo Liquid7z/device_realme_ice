bd023a3d12371d86085edfb82174ec5270bbdbe4
Liquid <101341072+Liquid7z@users.noreply.github.com>
2025-07-04 22:23:33 +0530

    Bring up for EVO

--- lineage_ice.mk
+++ lineage_ice.mk
@@ -12,9 +12,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/realme/ice/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/alpha/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-PRODUCT_NAME := alpha_ice
+PRODUCT_NAME := lineage_ice
 PRODUCT_DEVICE := ice
 PRODUCT_MANUFACTURER := realme
 PRODUCT_BRAND := realme
@@ -31,14 +31,15 @@ TARGET_SUPPORTS_OMX_SERVICE := false
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Flags
-ALPHA_BUILD_TYPE := Unofficial
-ALPHA_MAINTAINER := Liquid7z
+EVO_BUILD_TYPE := Unofficial
+EVO_MAINTAINER := Liquid7z
 TARGET_ENABLE_BLUR := true
 TARGET_EXCLUDES_AUDIOFX := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_BUILD_GOOGLE_TELEPHONY := true
 USE_MOTO_CALCULATOR := true
 TARGET_INCLUDE_LAWNCHAIR := true
+TARGET_SUPPORTS_QUICK_TAP := true
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc=$(call normalize-path-list, "RMX3461-user 13 TP1A.220905.001 R.1119f23-59b8-59b9 release-keys") \
